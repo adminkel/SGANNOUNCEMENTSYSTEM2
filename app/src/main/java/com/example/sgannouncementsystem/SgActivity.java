5ab592b3bf60faf0a0049dbc7035b4d39d0e381c
kelkel22 <abenoja.kelmorian@gmail.com>
2019-02-20 02:03:03 +0800

    test

---
+++ app/src/main/java/com/example/sgannouncementsystem/SgActivity.java
@@ -0,0 +1,13 @@
+package com.example.sgannouncementsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SgActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sg);
+    }
+}
