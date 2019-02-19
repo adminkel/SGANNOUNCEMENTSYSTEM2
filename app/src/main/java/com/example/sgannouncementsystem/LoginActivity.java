ff1002b7e1fa3cb07a6e189a65ed4c0c31a32f29
kelkel22 <abenoja.kelmorian@gmail.com>
2019-02-20 01:54:59 +0800

    test

--- app/src/main/java/com/example/sgannouncementsystem/LoginActivity.java
+++ app/src/main/java/com/example/sgannouncementsystem/LoginActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
