1e56a8a5327bcc6606d6e6c9da2f618ac57b9923
kelkel22 <abenoja.kelmorian@gmail.com>
2019-02-24 01:38:54 +0800

    test

--- app/src/main/java/com/example/sgannouncementsystem/StudentAdapter.java
+++ app/src/main/java/com/example/sgannouncementsystem/StudentAdapter.java
@@ -6,7 +6,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import java.text.SimpleDateFormat;
 import java.util.List;
 
@@ -18,7 +17,9 @@ public class StudentAdapter extends RecyclerView.Adapter<ViewHolder> {
     public StudentAdapter(StudentActivity studentActivity, List<Model> modelList) {
         this.studentActivity = studentActivity;
         this.modelList = modelList;
+
     }
+
 
     @NonNull
     @Override
